ece5fdbdd6b9465fe5707ac0739565f74ce4419d
Fad <fadhilerdyaqasmal@gmail.com>
2024-02-15 18:23:40 +0700

    membenarkan

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ const resetButton = document.getElementById("reset");
 const reverseButton = document.getElementById("reverse");
 
 
+
+let calcStatus = "ctof";
+
 const intConvert = (input) => {
     return parseInt(input);
 }
@@ -17,25 +20,69 @@ const intCheck = (inputCheck) => {
 }
 
 
-const ctof = () =>{
+const ctof = () => {
     const inputArea1 = document.getElementById("celsius");
+    const outputArea = document.getElementById("farenheit");
     const inputFound = inputArea1.value.trim();
     const numberInput = intConvert(inputFound);
-
-
 
     if(inputFound == ""){
         inputArea1.value = "Area ini tidak boleh kosong";
     }else if(!intCheck(numberInput)){
         inputArea1.value = "Tolong isi dengan angka";
     }else{
-        const outputArea = document.getElementById("farenheit");
         const fahrenheit = (numberInput * 9/5) + 32;
         outputArea.value = fahrenheit;  
     }
 }
 
+const ftoc = () => {
+    const inputArea1 = document.getElementById("celsius");
+    const inputFound = inputArea1.value.trim();
+    const outputArea = document.getElementById("farenheit");
+    const numberInput = intConvert(inputFound);
+
+    if(inputFound == ""){
+        inputArea1.value = "Area ini tidak boleh kosong";
+    }else if(!intCheck(numberInput)){
+        inputArea1.value = "Tolong isi dengan angka";
+    }else{
+        const fahrenheit = (numberInput - 32) * 5/9;
+        outputArea.value = fahrenheit;  
+    }
+}
+
+const reverse = () => {
+    const inputLabel = document.getElementById("label1");
+    const outputLabel = document.getElementById("label2");
+    const inputArea1 = document.getElementById("celsius");
+    const outputArea = document.getElementById("farenheit");
+    if(calcStatus == "ctof"){
+        calcStatus = "ftoc";
+        inputLabel.textContent = "Farenheit(F°)";
+        outputLabel.textContent = "Celsius(C°)";
+        inputArea1.value = outputArea.value;
+        ftoc();
+    }
+}
+
 conversionButton.addEventListener("click", () => {
     // Perform other actions as needed
-    ctof();
+    if (calcStatus == "ctof"){
+        ctof();
+    }else if(calcStatus == "ftoc"){
+        ftoc();
+    }
+    
   });
+
+reverseButton.addEventListener("click",() => {
+    reverse()
+})  
+
+resetButton.addEventListener("click", () => {
+    const inputArea1 = document.getElementById("celsius");
+    const outputArea = document.getElementById("farenheit");
+    inputArea1.value = ""
+    outputArea.value = ""
+})
